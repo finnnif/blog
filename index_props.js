2c1bc2d8cee108f54a1821331ee987d036f7c27d
finnnif <finnnif@users.noreply.github.com>
2020-06-27 01:06:05 +0000

    deploy: 89291141b2223076e321b3050e4715987bb83c03

---
+++ index_props.js
@@ -0,0 +1,60 @@
+
+export default {
+    'config': {
+        "srcDir": "src",
+        "publicDir": "public",
+        "ignore": [
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {}
+        ],
+        "base": "/blog/",
+        "theme": "docs",
+        "plugins": [
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        ],
+        "watch": false,
+        "serve": false,
+        "port": 8000,
+        "title": "finnnif's blog"
+    },
+    'pagePath': "index.md",
+    'layoutPath': "_layout.tsx",
+    'outputPath': "index.html",
+    'title': "Pagic",
+    'content': React.createElement("article", { dangerouslySetInnerHTML: {
+            __html: '<h1 id="pagic">Pagic <a class="header-anchor" href="#pagic">§</a></h1>\n<p>The easiest way to generate static html page from markdown, built with Deno! 🦕</p>\n'
+        } }),
+    'script': React.createElement(React.Fragment, null,
+        React.createElement("script", { crossOrigin: "anonymous", src: "https://unpkg.com/react@16.13.1/umd/react.production.min.js" }),
+        React.createElement("script", { crossOrigin: "anonymous", src: "https://unpkg.com/react-dom@16.13.1/umd/react-dom.production.min.js" }),
+        React.createElement("script", { src: "/blog/main.js", type: "module" }))
+};
