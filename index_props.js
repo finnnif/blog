65f0fc6160cdcdc56f3c8525615d98ac736effaf
finnnif <finnnif@users.noreply.github.com>
2020-06-27 01:46:45 +0000

    deploy: 0dd0ba8301cd0962cea8ff9d630933e41b659700

--- index_props.js
+++ index_props.js
@@ -1,4 +1,4 @@
-import projectConfig from '/blog/pagic.config.js';
+import projectConfig from '/pagic.config.js';
 export default {
     config: { "srcDir": "src", "publicDir": "public", "base": "/", ...projectConfig },
     'pagePath': "index.md",
@@ -11,7 +11,7 @@ export default {
     'script': React.createElement(React.Fragment, null,
         React.createElement("script", { crossOrigin: "anonymous", src: "https://unpkg.com/react@16.13.1/umd/react.production.min.js" }),
         React.createElement("script", { crossOrigin: "anonymous", src: "https://unpkg.com/react-dom@16.13.1/umd/react-dom.production.min.js" }),
-        React.createElement("script", { src: "/blog/main.js", type: "module" })),
+        React.createElement("script", { src: "/main.js", type: "module" })),
     'toc': React.createElement("aside", { dangerouslySetInnerHTML: {
             __html: '<nav class="toc"><ol></ol></nav>'
         } })
