15f2b1e253f56e827f9bc2f93729828cb7f97091
finnnif <finnnif@users.noreply.github.com>
2020-06-27 01:15:48 +0000

    deploy: d2686ece3151ddfdf2f19e04503a2ff06aaa552e

---
+++ pagic.config.js
@@ -0,0 +1,27 @@
+export default {
+    base: '/blog/',
+    srcDir: 'src',
+    publicDir: 'public',
+    // https://docs.npmjs.com/using-npm/developers.html#keeping-files-out-of-your-package
+    ignore: [
+        /\/\..+\.swp$/,
+        /\/\._/,
+        /\/\.DS_Store$/,
+        /\/\.git\//,
+        /\/\.hg\//,
+        /\/\.npmrc$/,
+        /\/\.lock-wscript$/,
+        /\/\.svn\//,
+        /\/\.wafpickle-.+/,
+        /\/config\.gypi$/,
+        /\/CVS\//,
+        /\/npm-debug\.log$/,
+        /\/node_modules\//
+    ],
+    theme: 'docs',
+    plugins: ['init', 'md', 'tsx', 'script', 'layout', 'write'],
+    watch: false,
+    serve: false,
+    port: 8000,
+    title: "finnnif's blog"
+};
